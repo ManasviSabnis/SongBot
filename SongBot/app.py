5c8067618acaf9bc55f5e7ccdd13fa623257f03c
Aman1496 <sheth.ad@somaiya.edu>
2022-03-24 21:43:34 +0530

    frontend

--- SongBot/app.py
+++ SongBot/app.py
@@ -1,32 +1,16 @@
-from flask import Flask, render_template, request, url_for, redirect, session,flash
-import pymongo
-import bcrypt
-from pymongo import MongoClient, ssl_support
-#set app as a Flask instance 
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
-#encryption relies on secret keys so they could be run
-#app.secret_key = "testing"
-#connoct to your Mongo DB database
-#mongo = MongoClient("mongodb+srv://manasvi:man14@queenman.rrrho.mongodb.net/test", ssl_cert_reqs=ssl_support.CERT_NONE)
+app.static_folder = 'static'
 
-#get the database name
-#db = mongo.get_database('reg_records')
-#get the particular collection that contains the data
-#records = db.register
-#test = mongo.get_database('prod_details')
-#details = test.prod_details
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-#assign URLs to have a particular route
-@app.route('/') 
-@app.route('/dashboard', methods=['POST'])
-def dashboard():
-    
-        return render_template('dashboard.html')
-    # except Exception as e:
-    #     return dumps({'error' : str(e)})
-
-
+# @app.route("/get")
+# def get_bot_response():
+#     userText = request.args.get('msg')
+#     return str(chatbot.get_response(userText))
 
 if __name__ == "__main__":
- 
-  app.run(host='0.0.0.0',debug=True)
+    app.run()
