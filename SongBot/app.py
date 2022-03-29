74a498ace017bcbb4fa6aff3ca30591ee1ddb4d9
Aman1496 <sheth.ad@somaiya.edu>
2022-03-29 20:05:33 +0530

    pages commit

--- SongBot/app.py
+++ SongBot/app.py
@@ -7,6 +7,17 @@ app.static_folder = 'static'
 def index():
     return render_template("index.html")
 
+@app.route("/bot")
+def bot():
+    return render_template("bot.html")
+
+@app.route("/feedback")
+def feedback():
+    return render_template("feedback.html")
+
+@app.route("/statistics")
+def statistics():
+    return render_template("statistics.html")
 # @app.route("/get")
 # def get_bot_response():
 #     userText = request.args.get('msg')
